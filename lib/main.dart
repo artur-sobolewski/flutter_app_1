6e6d6bc41360af133b7563f0acdb72369f87f75b
Artur <arturexd178@gmail.com>
2021-10-11 19:23:39 +0200

    Added class Question with separated widget

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,62 @@
-void main() {}
+import 'package:flutter/material.dart';
+import './question.dart';
 
-class MyApp {}
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    // TODO: implement createState
+    return _MyAppState();
+  }
+}
+
+class _MyAppState extends State<MyApp> {
+  var questionIdx = 0;
+
+  void answerQuestion() {
+    setState(() {
+      questionIdx += 1;
+    });
+    print(questionIdx);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    var questions = [
+      'What\'s your favorite color',
+      'What\'s your favorite animal?'
+    ];
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('My First App'),
+        ),
+        body: Column(
+          children: <Widget>[
+            Question(
+              questions[questionIdx],
+            ),
+            RaisedButton(
+              child: Text('Answer 1'),
+              onPressed: answerQuestion,
+            ),
+            RaisedButton(
+              child: Text('Answer 2'),
+              onPressed: () => print('Answer 2 chosen'),
+            ),
+            RaisedButton(
+              child: Text('Answer 3'),
+              onPressed: () {
+                //....
+                print('Answer 3 chosen');
+              },
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
